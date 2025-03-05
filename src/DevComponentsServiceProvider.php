ea5bf1995f0257a5ec8e60caa0729d8916ee988e
Tu Nombre <you@example.com>
2025-03-05 16:14:57 -0500

    test

--- src/DevComponentsServiceProvider.php
+++ src/DevComponentsServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Rusbelito\DevComponents;
+namespace DevComponents;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Blade;
 class DevComponentsServiceProvider extends ServiceProvider
 {
     /**
-     * Registra los servicios del aquete.
+     * Registra los servicios del paquete.
      *
      * @return void
      */
@@ -22,21 +22,36 @@ class DevComponentsServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot() : void
+    public function boot(): void
     {
-
         // Cargar vistas desde el directorio de tu paquete
-        $this->loadViewsFrom(__DIR__.'/resources/views/components', 'rusbelito');
+        $this->loadViewsFrom(__DIR__ . '/resources/views/components', 'dev-componets');
 
         // Publicar vistas para que puedan ser sobrescritas en el proyecto
         $this->publishes([
-            __DIR__.'/resources/views/components' => resource_path('views/components'),
+            __DIR__ . '/resources/views/components' => resource_path('views/components'),
         ]);
 
-        // Registrar el componente Blade para usar la sintaxis <rusbelito:componente />
-        Blade::componentNamespace('Rusbelito\\DevComponents\\Views\\Components', 'rusbelito');
+        // Registrar el componente Blade sin el prefijo 'x-'
+        $this->registerBladeComponents();
+    }
 
+    /**
+     * Registra los componentes Blade.
+     *
+     * @return void
+     */
+    protected function registerBladeComponents(){
+    // Obtener todos los archivos de vista de componentes
+    $componentFiles = glob(__DIR__ . '/resources/views/components/*.blade.php');
+
+    foreach ($componentFiles as $file) {
+        // Extraer el nombre del componente sin la extensión `.blade.php`
+        $componentName = basename($file, '.blade.php');
+
+        // Registrar el componente dinámicamente
+        Blade::component('dev-componets::components.' . $componentName, 'rusbelito:' . $componentName);
     }
 }
 
-
+}
